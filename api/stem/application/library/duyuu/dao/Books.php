169c40e02bc21b89e537797911308843d5c74d8d
jess <shawnisun@gmail.com>
2012-11-26 17:27:06 +0800

    API:user bug

--- api/stem/application/library/duyuu/dao/Books.php
+++ api/stem/application/library/duyuu/dao/Books.php
@@ -73,7 +73,7 @@ class Books extends \local\db\ORM
         $books = self::instance();
         $table = $this->table;
 
-        $list =  $books->field("books.bid,books.cid,i.path as cover,books.title,books.author")->joinQuery('book_category as c',"c.cid=$table.cid")->joinQuery('book_image as p',"$table.bid=p.bid")->joinQuery('images as i','i.pid=p.pid')->limit(10)->fetchList();
+        $list =  $books->field("books.bid,books.cid,i.path as cover,books.title,books.author,bi.apple_price as price")->joinQuery('book_category as c',"c.cid=$table.cid")->joinQuery('book_image as p',"$table.bid=p.bid")->joinQuery('images as i','i.pid=p.pid')->joinQuery('book_info as bi','bi.bid=books.bid')->limit(10)->fetchList();
         
         if (is_array($list)) {
             foreach ($list as $key => $value) {
@@ -116,7 +116,7 @@ class Books extends \local\db\ORM
         $list['count'] = $count;
         $list['page'] = $page;
         $list['pages'] = $pages;
-        $list["list"] = $books->field("$table.bid,$table.title,$table.author,i.path as cover")->joinQuery('book_image as p',"$table.bid=p.bid")->joinQuery('images as i','i.pid=p.pid')->where("p.type = 1 AND cid='$cid'")->limit("$offset,$limit")->fetchList();
+        $list["list"] = $books->field("$table.bid,$table.title,$table.author,i.path as cover,bi.apple_price as price")->joinQuery('book_image as p',"$table.bid=p.bid")->joinQuery('images as i','i.pid=p.pid')->joinQuery('book_info as bi',"bi.bid=$table.bid")->where("p.type = 1 AND cid='$cid'")->limit("$offset,$limit")->fetchList();
 
         if (is_array($list["list"])) {
             foreach ($list["list"] as $key => $value) {
@@ -143,7 +143,7 @@ class Books extends \local\db\ORM
                     $list[$key]['cover'] = \duyuu\image\ImageControl::getRelativeImage($value['cover']);
                 }
             }
-            return $list;
+            return $list[0];
         }
 
         return "";
