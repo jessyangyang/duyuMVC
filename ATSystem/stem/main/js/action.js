7fbf1b2848e02899f3f8fa0eafbdd80b7d3beade
jess <shawnisun@gmail.com>
2013-05-23 18:00:59 +0800

    script

--- ATSystem/stem/main/js/action.js
+++ ATSystem/stem/main/js/action.js
@@ -1,10 +1,10 @@
 ;$(function(){
-    $("body").on('submit','.form-horizontal',function(){
-        $(".form-horizontal").ajaxSubmit({
-            target: 'body'
-        });
-        // return false; 
-    });
+    // $("body").on('submit','.form-horizontal',function(){
+    //     $(".form-horizontal").ajaxSubmit({
+    //         target: 'body'
+    //     });
+    //     return false; 
+    // });
     $("body").on('click',".btn-next,.btn-commit",function(){
         $(".form-horizontal").submit();
         return false; 
