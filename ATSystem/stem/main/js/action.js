d8d6ae19b7f8ffdb0f8ea16f83fb91d0504b4949
jess <shawnisun@gmail.com>
2013-05-24 18:31:14 +0800

    ADD: edit arrow.png and fixed.css

--- ATSystem/stem/main/js/action.js
+++ ATSystem/stem/main/js/action.js
@@ -9,6 +9,23 @@
         $(".form-horizontal").submit();
         return false; 
     });
+    $("body").on('click','.edit-setting-title',function(){
+        $(".edit-title-subitem").slideToggle('fast',function(){
+            $(".edit-setting-title-img").css("transform",function(index,value)
+            {
+
+                if(value !== 'none')
+                {
+                    var values = value.split('(')[1].split(')')[0].split(',');
+                    var a = values[0];
+                    var b = values[1];
+                    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+                    if (angle == 0) return "rotate(-180deg)";
+                    else return "rotate(-0deg)";
+                }
+            });
+        });
+    });
 
     
 });
