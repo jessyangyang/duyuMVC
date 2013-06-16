5ecc2d4fb885f054af0d530644bef3b334559545
jess <shawnisun@gmail.com>
2013-06-17 02:00:43 +0800

    common files

--- common/stem/local/files/Files.php
+++ common/stem/local/files/Files.php
@@ -12,5 +12,29 @@ namespace local\files;
 
 class Files extends \Filesystem
 {
-    
+    /**
+     * Make New DIR
+     *
+     * Only add new dir  in files menu.
+     *
+     * Example: "/home/wwwroot/duyuMVC/files" after add new dir.
+     *
+     * @param $path String 
+     * @param $mode Int
+     *
+     * @return Boolean
+     */
+    static public function makeNewDir($path,$mode = 0755)
+    {
+        $pathTmp = explode('/',$path);
+        $newDir = "";
+        $is_dir = false;
+        foreach ($pathTmp as $key => $value) {
+            $newDir .= '/' . $value;
+            if (!is_dir($newDir)) {
+                $is_dir = mkdir($newDir,$mode);
+            }
+        }
+        return $is_dir;
+    }
 }
