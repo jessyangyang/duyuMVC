0207e9e747f004bb0fcf2f96eec3183a6460c9a9
jess <shawnisun@gmail.com>
2013-06-17 02:00:09 +0800

    models move to common

---
+++ common/stem/local/models/Members.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* Members Model 
+*
+* @package     DuyuMvc
+* @author      Jess
+* @version     1.0
+* @license     http://wiki.duyu.com/duyuMvc
+*/
+
+namespace local\models;
+
+class Members extends \local\db\ORM 
+{
+    public $table = 'members';
+
+    public $fields = array(
+        'id' => array(
+            'type' => 'int',
+            'default' => 0,
+            'comment' => 'bookID'),
+        'email' => array(
+            'type' => 'varchar',
+            'default' => 0,
+            'comment' => 'email'),
+        'password' => array(
+            'type' => 'varchar',
+            'default' => 0,
+            'comment' => 'password'),
+        'username' => array(
+            'type' => 'varchar',
+            'default' => 0,
+            'comment' => 'username'),
+        'published' => array(
+            'type' => 'int',
+            'default' => 0,
+            'comment' => 'published'),
+        'avatar_id' => array(
+            'type' => 'int',
+            'default' => 0,
+            'comment' => 'avatar_id'),
+        'role_id' => array(
+            'type' => 'int',
+            'default' => 0,
+            'comment' => 'role_id')
+        );
+
+    public $primaryKey = "id";
+
+    protected static $instance;
+
+    /**
+     * Instance Members
+     * @param  boolean $id | the primaryKey
+     * @return Object with self
+     */
+    public static function instance($id = false)
+    {
+        return self::$instance ? self::$instance : new Members($id);
+    }
+
+    /**
+     * [getByID description]
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public static function getByID($id)
+    {
+        return self::instance($id);
+    }
+}
