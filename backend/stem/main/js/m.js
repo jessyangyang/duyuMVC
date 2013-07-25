692d53bea35574b11b9df128f1a2f690c95c89de
jess <shawnisun@gmail.com>
2013-07-26 00:20:02 +0800

    index

--- backend/stem/main/js/m.js
+++ backend/stem/main/js/m.js
@@ -11,10 +11,10 @@
         //获取滚动条的滑动距离
         if(WinscroH>=WindowsMenuH){
             //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
-            $("nav").addClass("fixed-layer");
+            $("#header").addClass("fixed-layer");
         //  $(".search-blue-line-bg").css({"position":"fixed","top":"0","left":"50%","margin-left":"-500px","width":"1000px"});
         }else if(WinscroH<WindowsMenuH){
-            $("nav").removeClass("fixed-layer");
+            $("#header").removeClass("fixed-layer");
         //  $(".search-blue-line-bg").css({"position":"static"});
         }
     })
