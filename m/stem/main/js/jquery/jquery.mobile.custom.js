82033c4b5d1705f61b2f491ab5bb420e85227e3c
jess <shawnisun@gmail.com>
2013-08-02 10:49:58 +0800

    js

---
+++ m/stem/main/js/jquery/jquery.mobile.custom.js
@@ -0,0 +1,10 @@
+;$(function(){
+    $(document).bind("mobileinit", function()  
+    {  
+       if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('iPad') != -1 || navigator.userAgent.indexOf('Blackberry') != -1 || navigator.userAgent.indexOf('Android') != -1) {
+            $.mobile.defaultPageTransition = 'none'; 
+            $.mobile.defaultDialogTransition = 'none';
+        }
+        $.mobile.page.prototype.options.keepNative = "input, textarea";
+    }); 
+});
